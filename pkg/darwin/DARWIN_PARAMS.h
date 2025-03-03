31577d43dc822d67fe89074f90bc7e95211fcc3d
Oliver Jahn <jahn@mit.edu>
2025-03-03 10:39:36 -0500

    Add RADI v2

--- pkg/darwin/DARWIN_PARAMS.h
+++ pkg/darwin/DARWIN_PARAMS.h
@@ -116,7 +116,7 @@ C     oC0 :: Coefficient for determining saturation O2
       _RL oB3
       _RL oC0
 
-# ifdef DARWIN_ALLOW_RADI
+# ifdef DARWIN_ALLOW_RADIv1
 C--   COMMON /DARWIN_SED_CONSTANTS_R/ Coefficients for sediment model
 C     sed_a1 :: coefficient for sediment model
 C     sed_b1 :: coefficient for sediment model
@@ -182,6 +182,103 @@ C     sed_c :: coefficient for sediment model
       _RL sed_c5
       _RL sed_d5
       _RL sed_c
+# endif
+
+# ifdef DARWIN_ALLOW_RADIv2
+C--   COMMON /DARWIN_SED_CONSTANTS_r/ Coefficients for sediment model
+C     sed_depth_threshold :: depth threshold for deep/coast equations
+C     sed_a1              :: coefficient for sediment model
+C     sed_b1              :: coefficient for sediment model
+C     sed_c1              :: coefficient for sediment model
+C     sed_d1              :: coefficient for sediment model
+C     sed_e1              :: coefficient for sediment model
+C     sed_a2              :: coefficient for sediment model
+C     sed_b2              :: coefficient for sediment model
+C     sed_c2              :: coefficient for sediment model
+C     sed_d2              :: coefficient for sediment model
+C     sed_e2              :: coefficient for sediment model
+C     sed_a3              :: coefficient for sediment model
+C     sed_b3              :: coefficient for sediment model
+C     sed_c3              :: coefficient for sediment model
+C     sed_d3              :: coefficient for sediment model
+C     sed_e3              :: coefficient for sediment model
+C     sed_a4              :: coefficient for sediment model
+C     sed_b4              :: coefficient for sediment model
+C     sed_c4              :: coefficient for sediment model
+C     sed_d4              :: coefficient for sediment model
+C     sed_e4              :: coefficient for sediment model
+C     sed_a5              :: coefficient for sediment model
+C     sed_b5              :: coefficient for sediment model
+C     sed_c5              :: coefficient for sediment model
+C     sed_d5              :: coefficient for sediment model
+C     sed_e5              :: coefficient for sediment model
+C     sed_a6              :: coefficient for sediment model
+C     sed_b6              :: coefficient for sediment model
+C     sed_c6              :: coefficient for sediment model
+C     sed_d6              :: coefficient for sediment model
+C     sed_e6              :: coefficient for sediment model
+
+      COMMON /DARWIN_SED_CONSTANTS_r/
+     &    sed_globala1,
+     &    sed_globalb1,
+     &    sed_globalc1,
+     &    sed_globald1,
+     &    sed_globale1,
+     &    sed_globala2,
+     &    sed_globalb2,
+     &    sed_globalc2,
+     &    sed_globald2,
+     &    sed_globale2,
+     &    sed_globala3,
+     &    sed_globalb3,
+     &    sed_globalc3,
+     &    sed_globald3,
+     &    sed_globale3,
+     &    sed_globala4,
+     &    sed_globalb4,
+     &    sed_globalc4,
+     &    sed_globald4,
+     &    sed_globale4,
+     &    sed_globala5,
+     &    sed_globalb5,
+     &    sed_globalc5,
+     &    sed_globald5,
+     &    sed_globale5,
+     &    sed_globala6,
+     &    sed_globalb6,
+     &    sed_globalc6,
+     &    sed_globald6,
+     &    sed_globale6
+      _RL sed_globala1
+      _RL sed_globalb1
+      _RL sed_globalc1
+      _RL sed_globald1
+      _RL sed_globale1
+      _RL sed_globala2
+      _RL sed_globalb2
+      _RL sed_globalc2
+      _RL sed_globald2
+      _RL sed_globale2
+      _RL sed_globala3
+      _RL sed_globalb3
+      _RL sed_globalc3
+      _RL sed_globald3
+      _RL sed_globale3
+      _RL sed_globala4
+      _RL sed_globalb4
+      _RL sed_globalc4
+      _RL sed_globald4
+      _RL sed_globale4
+      _RL sed_globala5
+      _RL sed_globalb5
+      _RL sed_globalc5
+      _RL sed_globald5
+      _RL sed_globale5
+      _RL sed_globala6
+      _RL sed_globalb6
+      _RL sed_globalc6
+      _RL sed_globald6
+      _RL sed_globale6
 # endif
 #endif
 
